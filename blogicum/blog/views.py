e29cc0d978734d1a7a4e50d868e85853f8a46ada
Богданов Артём <bogdartem1997@yandex.ru>
2023-04-18 22:43:19 +0400

    Add views functions for blog app.

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -45,3 +45,13 @@ posts = [
     },
 ]
 
+
+def index(request):
+    template = 'blog/index.html'
+    return render(request, template)
+
+
+def post_detail(request, pk):
+    template = 'blog/detail.html'
+    context = {'posts': posts[pk]}
+    return render(request, template, context)
